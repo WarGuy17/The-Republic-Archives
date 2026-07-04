450dc5cf3cadc8d285a5d78ede81ae74a205a31c
Justin Tweed <tweedjustin@gmail.com>
2026-07-03 23:04:50 -0400

    Added a new html for people and started CSS with it

--- index.js
+++ index.js
@@ -1,19 +1,2 @@
-async function getStarshipManifestAsync(){
-    try {
-        const response = await fetch('https://swapi.dev/api/starships/?page=1');
+const cardArea = document.querySelector('#card-area');
 
-        if(!response.ok){
-            throw new Error("Ship Manifest didn't load properly");
-        }
-
-        const data = await response.json();
-
-        console.log(data)
-    }
-    catch(error){
-        console.error("There was an error" + error)
-    }
-}
-console.log("Archive boot sequence initiated")
-
-getStarshipManifestAsync();
